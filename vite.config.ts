c526dfc5d1a86f71270490292e3c3752e7f0f04c
Jose Polo <jfer678@gmail.com>
2026-05-25 10:33:34 -0500

    FINAL

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: { exclude: ["react-connect-lines"] },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+});
